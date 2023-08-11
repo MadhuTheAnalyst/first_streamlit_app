a29d5fd814ab3bed980c0057d05e2f2e584c1cbb
MadhuTheAnalyst <141990360+MadhuTheAnalyst@users.noreply.github.com>
2023-08-11 11:55:35 +0530

    Update streamlit_app.py
    imported pandas and read data from csv and converted it into a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas as pd
 streamlit.title('My parents new healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -8,3 +8,5 @@ streamlit.text('🥗 kale,spinach and rocket smoothie')
 streamlit.text('🐔Hard Boiled free range egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlist.dataframe(my_fruit_list)
